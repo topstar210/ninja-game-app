04d92356d11b94e360633a90f87f2a472f057ed3
topstar210 <happycamper0210@gmail.com>
2023-03-25 00:39:35 +0900

    no needed code delete

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,9 +3,5 @@
         'data-aos-mirror': true,
         'data-aos-once': false
     });
-    const scroller = new LocomotiveScroll({
-        el: document.querySelector('[data-scroll-container]'),
-        smooth: true
-    });
 
 })();
